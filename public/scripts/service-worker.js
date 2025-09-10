f29bf01b6f922acf2717ae2ded5b5cd866f550f2
Stefan Hasenoot <stefanhasenoot@hotmail.com>
2025-09-10 14:04:05 +0200

    Update service-worker.js

--- public/scripts/service-worker.js
+++ public/scripts/service-worker.js
@@ -1,8 +1,8 @@
 self.addEventListener('push', function(event) {
     const options = {
         body: event.data ? event.data.text() : 'Blood Notification',
-        icon: '/assets/sad_blood.jpg',
-        badge: '/assets/sad_blood.jpg'
+        icon: 'assets/sad_blood.jpg',
+        badge: 'assets/sad_blood.jpg'
     };
 
     event.waitUntil(
